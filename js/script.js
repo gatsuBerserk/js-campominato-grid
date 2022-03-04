dce85d652d95dc3ed4016dc6e802276d5f6f4dd9
gatsuBerserk <berserkdev@outlook.it>
2022-03-04 16:12:49 +0100

    grid creation with function

--- js/script.js
+++ js/script.js
@@ -10,6 +10,27 @@
         // 1) Creiamo un solo quadrato e inseriamolo nel DOM 
             // Prendiamo la posizione nel quale andrà inserito l'elemento
             const domGrid = document.getElementById("grid"); 
-            const element = document.createElement("div");
-            element.classList.add("my-square"); 
-            domGrid.appendChild(element);
+            // Creiamo l'elemento 
+            // const element = document.createElement("div");
+            // // Aggiungiamo una classe 
+            // element.classList.add("my-square"); 
+            // // Inseriamolo nel DOM
+            // domGrid.appendChild(element); 
+            
+            //creiamo più elementi attraverso una funzione 
+            function createGrid() {
+                const element = document.createElement("div");
+                element.classList.add("my-square"); 
+                return element;
+            };
+
+            for(let i = 0; i < 100; i ++){
+                const singleSquare = createGrid(); 
+                singleSquare.addEventListener("click", function(){
+                singleSquare.classList.toggle("bg-warning");
+                console.log(singleSquare); 
+                })
+                
+                domGrid.appendChild(singleSquare);
+
+            };
