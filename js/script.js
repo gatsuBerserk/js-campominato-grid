5af84be05316cd8847d72df2fcb696b7506ec5a4
gatsuBerserk <berserkdev@outlook.it>
2022-03-04 17:17:37 +0100

    added event for levels

--- js/script.js
+++ js/script.js
@@ -9,7 +9,13 @@
     // Atomicità 
         // 1) Creiamo un solo quadrato e inseriamolo nel DOM 
             // Prendiamo la posizione nel quale andrà inserito l'elemento
-            const domGrid = document.getElementById("grid"); 
+            const domGrid = document.getElementById("grid");
+               // Prendiamo dal DOM la selezione della difficoltà    
+            const gameDifficulty = document.getElementById("difficulty");
+                // Prendiamo il Bottone Dal DOM 
+            const play =document.getElementById("play");  
+            
+            
             // Creiamo l'elemento 
             // const element = document.createElement("div");
             // // Aggiungiamo una classe 
@@ -17,20 +23,61 @@
             // // Inseriamolo nel DOM
             // domGrid.appendChild(element); 
             
-            //creiamo più elementi attraverso una funzione 
-            function createGrid() {
-                const element = document.createElement("div");
+            //Creaimo l'elemento div con funzione 
+            function createGrid() { 
+                // Creazione elemento "div"
+                const element = document.createElement("div"); 
+                // Aggiunta classe
                 element.classList.add("my-square"); 
                 return element;
             };
+            
+            
 
-            for(let i = 0; i < 100; i ++){
-                const singleSquare = createGrid(); 
-                singleSquare.addEventListener("click", function(){
-                singleSquare.classList.toggle("bg-warning");
-                console.log(singleSquare); 
-                })
+            play.addEventListener("click", function(){ 
+                domGrid.innerHTML="";
+                if(gameDifficulty.value === "easy"){
+                    for(let i = 0; i < 100; i ++){ 
+                        // Singolo quadrato 
+                        const singleSquare = createGrid(); 
+                        // Aggiungiamo un evento  
+                        singleSquare.addEventListener("click", function(){ 
+                        // Aggiungiamo la classe 
+                        singleSquare.classList.toggle("bg-warning");
+                        console.log(singleSquare); 
+                        })
+                        // Inseriamo gli elementi nel DOM
+                        domGrid.appendChild(singleSquare);
+        
+                    }
+
+                    } else if(gameDifficulty.value === "medium"){
+                        for(let i = 0; i < 80; i ++){ 
+                            // Singolo quadrato 
+                            const singleSquare = createGrid(); 
+                            // Aggiungiamo un evento  
+                            singleSquare.addEventListener("click", function(){ 
+                            // Aggiungiamo la classe 
+                            singleSquare.classList.toggle("bg-warning");
+                            console.log(singleSquare); 
+                            })
+                            // Inseriamo gli elementi nel DOM
+                            domGrid.appendChild(singleSquare);
+            
+                        }
+                    } else{
+                            for(let i = 0; i < 40; i ++){ 
+                                // Singolo quadrato 
+                                const singleSquare = createGrid(); 
+                                // Aggiungiamo un evento  
+                                singleSquare.addEventListener("click", function(){ 
+                                // Aggiungiamo la classe 
+                                singleSquare.classList.toggle("bg-warning");
+                                console.log(singleSquare); 
+                                })
+                                // Inseriamo gli elementi nel DOM
+                                domGrid.appendChild(singleSquare);
                 
-                domGrid.appendChild(singleSquare);
-
-            };
+                            }   
+                    }
+            });
