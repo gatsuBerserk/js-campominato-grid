b004c710a0df6e2df478988aa441220809060afb
gatsuBerserk <berserkdev@outlook.it>
2022-03-04 15:50:57 +0100

    First square of the grid

--- js/script.js
+++ js/script.js
@@ -4,4 +4,12 @@
         con difficoltà 2 => tra 1 e 81
         con difficoltà 3 => tra 1 e 49
         Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.
- */
+ */ 
+ 
+    // Atomicità 
+        // 1) Creiamo un solo quadrato e inseriamolo nel DOM 
+            // Prendiamo la posizione nel quale andrà inserito l'elemento
+            const domGrid = document.getElementById("grid"); 
+            const element = document.createElement("div");
+            element.classList.add("my-square"); 
+            domGrid.appendChild(element);
